5f98631422b9e20ec78ba7c5728165717ce7d093
elhajjaji.riad <riad@gmail.com>
2023-06-04 16:33:46 -0400

    ++

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,31 @@
-import express from "express";
-import dotenv from "dotenv";
-// import mongoose, { ConnectOptions } from "mongoose";
-import mainRouter from "./routes/api/mainRoute";
+// import express from "express";
+// import dotenv from "dotenv";
+// // import mongoose, { ConnectOptions } from "mongoose";
+// import mainRouter from "./routes/api/mainRoute";
 
-dotenv.config();
+// dotenv.config();
 
-const app = express();
+// const app = express();
 
-app.use(express.json());
+// app.use(express.json());
 
-// routes
-app.use("/api/email", mainRouter);
+// // routes
+// app.use("/api/email", mainRouter);
 
-// database connection
-// mongoose
-//   .connect(
-//     process.env.MONGO_URI as string,
-//     {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true
-//     } as ConnectOptions
-//   )
-//   .then(() => {
-//     console.log("Connected to database");
-//   })
-//   .catch((error: Error) => {
-//     console.log(`Database connection error: ${error.message}`);
-//   });
+// // database connection
+// // mongoose
+// //   .connect(
+// //     process.env.MONGO_URI as string,
+// //     {
+// //       useNewUrlParser: true,
+// //       useUnifiedTopology: true
+// //     } as ConnectOptions
+// //   )
+// //   .then(() => {
+// //     console.log("Connected to database");
+// //   })
+// //   .catch((error: Error) => {
+// //     console.log(`Database connection error: ${error.message}`);
+// //   });
 
-export default app;
+// export default app;
